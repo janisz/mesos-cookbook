ba80a483995ebadd372522b4b9a83870a86a7909
Tomasz Janiszewski <janiszt@gmail.com>
2016-08-02 19:36:36 +0200

    Use marshaller

--- 4_understanding_frameworks/scheduler.go
+++ 4_understanding_frameworks/scheduler.go
@@ -8,34 +8,52 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"github.com/golang/protobuf/jsonpb"
+	"os"
 )
 
 const master = "http://10.10.10.10:5050"
 const path = "/api/v1/scheduler"
 
+var marshaller = jsonpb.Marshaler{
+	EnumsAsInts: false,
+	Indent: "  ",
+	OrigName: true,
+}
+
+type scheduler struct {
+
+}
+
 func main() {
 	user := "root"
 	name := "simple_framework"
-	subscribe(FrameworkInfo{
+	hostname, err := os.Hostname()
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	log.Fatal(subscribe(FrameworkInfo{
 		User: &user,
 		Name: &name,
-	})
+		Hostname: &hostname,
+	}))
 }
 
 func subscribe(frameworkInfo FrameworkInfo) error {
-	body, err := json.Marshal(struct {
-		Type      string         `json:"type"`
-		Subscribe Call_Subscribe `json:"subscribe"`
-	}{
-		Type:      Call_SUBSCRIBE.String(),
-		Subscribe: Call_Subscribe{FrameworkInfo: &frameworkInfo},
-	})
+
+	subscribeCall := &Call{
+		Type: Call_SUBSCRIBE.Enum(),
+		Subscribe: &Call_Subscribe{FrameworkInfo: &frameworkInfo},
+	}
+
+	body, err := marshaller.MarshalToString(subscribeCall)
 
 	if err != nil {
 		log.Printf("%v Error: %v", log.Llongfile, err)
 		return err
 	}
-	res, err := http.Post(master+path, "application/json", bytes.NewBuffer([]byte(body)))
+	log.Print(body)
+	res, err := http.Post(master + path, "application/json", bytes.NewBuffer([]byte(body)))
 	if err != nil {
 		log.Printf("%v Error: %v", log.Llongfile, err)
 		return err
@@ -62,5 +80,12 @@ func subscribe(frameworkInfo FrameworkInfo) error {
 		var sub Event
 		json.Unmarshal([]byte(data), &sub)
 		log.Printf("Got: [%s]", sub.String())
+
+		switch *sub.Type {
+		case Event_HEARTBEAT: log.Print("PING")
+		case Event_OFFERS: log.Print("Offer")
+		case Event_OFFERS: log.Print("Offer")
+		}
+
 	}
 }
