69a631f5f154c7097875f2594f088551bb483b9f
Tomasz Janiszewski <janiszt@gmail.com>
2016-08-04 23:30:10 +0200

    Schedule tasks

--- 4_understanding_frameworks/scheduler.go
+++ 4_understanding_frameworks/scheduler.go
@@ -4,14 +4,16 @@ import (
 	"bufio"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
+
 	"github.com/golang/protobuf/jsonpb"
-	"os"
-	"io"
-	"fmt"
+	"sync/atomic"
 )
 
 const master = "http://10.10.10.10:5050"
@@ -19,34 +21,52 @@ const path = "/api/v1/scheduler"
 
 var marshaller = jsonpb.Marshaler{
 	EnumsAsInts: false,
-	Indent: "  ",
-	OrigName: true,
+	Indent:      "  ",
+	OrigName:    true,
 }
-var mesosStreamId string
+var mesosStreamID string
 
-type scheduler struct {
+var frameworkInfo *FrameworkInfo
 
-}
+var commandChan = make(chan string, 100)
+
+var taskId uint64
 
 func main() {
 	user := "root"
 	name := "simple_framework"
 	hostname, err := os.Hostname()
-	if (err != nil) {
+	if err != nil {
 		log.Fatal(err)
 	}
-	log.Fatal(subscribe(FrameworkInfo{
-		User: &user,
-		Name: &name,
+
+	frameworkInfo = &FrameworkInfo{
+		User:     &user,
+		Name:     &name,
 		Hostname: &hostname,
-	}))
+	}
+
+	go subscribe()
+	http.HandleFunc("/", web)               // set router
+	err = http.ListenAndServe(":9090", nil) // set listen port
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
 
-func subscribe(frameworkInfo FrameworkInfo) error {
+func web(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	cmd := r.Form["cmd"][0]
+	commandChan <- cmd
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprintf(w, "Scheduled: %s", cmd)
+}
+
+func subscribe() error {
 
 	subscribeCall := &Call{
-		Type: Call_SUBSCRIBE.Enum(),
-		Subscribe: &Call_Subscribe{FrameworkInfo: &frameworkInfo},
+		Type:      Call_SUBSCRIBE.Enum(),
+		Subscribe: &Call_Subscribe{FrameworkInfo: frameworkInfo},
 	}
 
 	body, err := marshaller.MarshalToString(subscribeCall)
@@ -88,44 +108,115 @@ func subscribe(frameworkInfo FrameworkInfo) error {
 		case Event_SUBSCRIBED:
 			log.Print("Subscribed")
 			frameworkInfo.Id = sub.Subscribed.FrameworkId
-			mesosStreamId = res.Header.Get("Mesos-Stream-Id")
-		case Event_HEARTBEAT: log.Print("PING")
-		case Event_OFFERS: {
+			mesosStreamID = res.Header.Get("Mesos-Stream-Id")
+		case Event_HEARTBEAT:
+			log.Print("PING")
+		case Event_OFFERS:
+			{
 
-			offerIds := []*OfferID{}
-			for _, offer := range sub.Offers.Offers {
-				offerIds = append(offerIds, offer.Id)
-			}
+				offerIds := []*OfferID{}
+				for _, offer := range sub.Offers.Offers {
+					offerIds = append(offerIds, offer.Id)
+				}
 
-			decline := &Call{
-				Type: Call_DECLINE.Enum(),
-				FrameworkId: frameworkInfo.Id,
-				Decline: &Call_Decline{OfferIds: offerIds},
+				select {
+				case cmd := <-commandChan:
+					firstOffer := sub.Offers.Offers[0]
+
+					TRUE := true
+					newTaskId := fmt.Sprint(atomic.AddUint64(&taskId, 1))
+					accept := &Call{
+						Type: Call_ACCEPT.Enum(),
+						Accept: &Call_Accept{
+							OfferIds: offerIds,
+							Operations: []*Offer_Operation{{
+								Type: Offer_Operation_LAUNCH.Enum(),
+								Launch: &Offer_Operation_Launch{
+									TaskInfos: []*TaskInfo{
+										{
+											Name: &cmd,
+											TaskId: &TaskID{Value: &newTaskId},
+											AgentId: firstOffer.AgentId,
+											Resources: defaultResources(),
+											Command: &CommandInfo{
+												Shell: &TRUE,
+												Value: &cmd,
+											},
+										},
+									},
+								},
+							},
+							},
+						},
+					}
+					err = call(accept)
+					if err != nil {
+						return err
+					}
+
+
+				default:
+					decline := &Call{
+						Type:    Call_DECLINE.Enum(),
+						Decline: &Call_Decline{OfferIds: offerIds},
+					}
+					err = call(decline)
+					if err != nil {
+						return err
+					}
+				}
 			}
-			body, err := marshaller.MarshalToString(decline)
-			if err != nil {
-				log.Printf("%v Error: %v", log.Llongfile, err)
-				return err
-			}
-			log.Print(body)
-			req, err := http.NewRequest("POST", master + path, bytes.NewBuffer([]byte(body)))
-			if (err != nil) {
-				return err
-			}
-			req.Header.Set("Mesos-Stream-Id", mesosStreamId)
-			req.Header.Set("Content-Type", "application/json")
-			res, err := http.DefaultClient.Do(req)
-			if err != nil {
-				log.Printf("%v Error: %v", log.Llongfile, err)
-				return err
-			}
-			if (res.StatusCode != 202) {
-				io.Copy(os.Stderr, res.Body)
-				return fmt.Errorf("%s", "Error")
-			}
-			res.Body.Close()
-		}
 		}
 
 	}
+}
+
+func defaultResources() []*Resource {
+	CPU := "cpus"
+	MEM := "mem"
+	cpu := float64(0.1)
+
+	return []*Resource{
+		{
+			Name: &CPU,
+			Type: Value_SCALAR.Enum(),
+			Scalar: &Value_Scalar{Value: &cpu},
+		},
+		{
+			Name: &MEM,
+			Type: Value_SCALAR.Enum(),
+			Scalar: &Value_Scalar{Value: &cpu},
+		},
+	}
+
+}
+
+// call Sends request to Mesos.
+// Returns nil if request was accepted in other case returns error
+func call(message *Call) error {
+	message.FrameworkId = frameworkInfo.Id
+	body, err := marshaller.MarshalToString(message)
+	if err != nil {
+		log.Printf("%v Error: %v", log.Llongfile, err)
+		return err
+	}
+	req, err := http.NewRequest("POST", master + path, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Mesos-Stream-Id", mesosStreamID)
+	req.Header.Set("Content-Type", "application/json")
+	log.Printf("Call %s %s", message.Type, string(body))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("%v Error: %v", log.Llongfile, err)
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 202 {
+		io.Copy(os.Stderr, res.Body)
+		return fmt.Errorf("Error %d", res.StatusCode)
+	}
+
+	return nil
 }
